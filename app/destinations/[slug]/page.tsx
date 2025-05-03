938a1a46a61a24211dd966efc11236ef8d385acc
Mohd faizan Hussain <faizanhussain78612@gmail.com>
2025-05-03 12:25:28 +0530

    done

--- app/destinations/[slug]/page.tsx
+++ app/destinations/[slug]/page.tsx
@@ -1,17 +1,11 @@
 import DestinationDetails from "@/app/components/Destination/DestinationDetails";
 
-// Correct interface for PageProps
-interface PageProps {
-  params: { slug: string };
-  searchParams?: { [key: string]: string | string[] | undefined };
-}
 
-export default async function Page({ params, searchParams }: PageProps) {
-  const { slug } = params;
 
-  const res = await fetch(`https://craftedvacays.com/wp-json/wp/v2/pages?slug=${slug}`);
+export default async function Page({ params }) {
+  const res = await fetch(`https://craftedvacays.com/wp-json/wp/v2/pages?slug=${params.slug}`);
   const data = await res.json();
   const destination = data?.[0]; // Get the first destination if available
 
-  return <DestinationDetails parentName={slug} destination={destination} />;
+  return <DestinationDetails parentName={params.slug} destination={destination} />;
 }
